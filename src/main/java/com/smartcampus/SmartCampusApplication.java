411807aa3512b3fe7eb9d3e93c4b92d338f9f8b8
CrishalLiyanageCS <crishalliyanage.cs825@gmail.com>
2026-04-23 20:34:57 +0530

    refactor: switch to standard JAX-RS Application class and simplify web.xml

--- src/main/java/com/smartcampus/SmartCampusApplication.java
+++ src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -1,12 +1,32 @@
 package com.smartcampus;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
-import org.glassfish.jersey.server.ResourceConfig;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api/v1")
-public class SmartCampusApplication extends ResourceConfig {
+public class SmartCampusApplication extends Application {
 
-    public SmartCampusApplication() {
-        packages("com.smartcampus");
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+
+        // Resource classes
+        classes.add(com.smartcampus.resource.DiscoveryResource.class);
+        classes.add(com.smartcampus.resource.RoomResource.class);
+        classes.add(com.smartcampus.resource.SensorResource.class);
+
+        // Exception mappers
+        classes.add(com.smartcampus.exception.mapper.GenericExceptionMapper.class);
+        classes.add(com.smartcampus.exception.mapper.LinkedResourceNotFoundExceptionMapper.class);
+        classes.add(com.smartcampus.exception.mapper.ResourceNotFoundExceptionMapper.class);
+        classes.add(com.smartcampus.exception.mapper.RoomNotEmptyExceptionMapper.class);
+        classes.add(com.smartcampus.exception.mapper.SensorUnavailableExceptionMapper.class);
+
+        // Filters
+        classes.add(com.smartcampus.filter.LoggingFilter.class);
+
+        return classes;
     }
 }
